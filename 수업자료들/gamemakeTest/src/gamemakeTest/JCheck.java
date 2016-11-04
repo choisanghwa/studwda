251084be5053d81daff9a555eabcfd221364f32e
choisanghwa <bdg9760@gmail.com>
2016-11-04 20:46:00 +0900

    Revert "Revert "게임 만들기""
    This reverts commit 304b3b8f6392c779374285a866fc8b2603ba113c.

---
+++ 수업자료들/gamemakeTest/src/gamemakeTest/JCheck.java
@@ -0,0 +1,13 @@
+package gamemakeTest;
+
+import javax.swing.*;
+
+public class JCheck extends JFrame{
+	static int i;
+	public JCheck(){
+		i=1;
+		setTitle("check");
+		setSize(300,300);
+		setVisible(true);
+	}
+}
